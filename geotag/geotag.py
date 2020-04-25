e588650996a30fefb98e3f9cc48e850828b69915
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2020-04-25 19:16:27 +0200

    ✨ automatically remove lonely outliers from location history

--- geotag/geotag.py
+++ geotag/geotag.py
@@ -3,6 +3,7 @@ import bisect
 import json
 import logging
 import os
+import math
 import subprocess
 import sys
 import tempfile
@@ -23,6 +24,57 @@ class Location:
     latitude: float
     longitude: float
     accuracy: int
+
+
+def dist_gps(lat1: float, long1: float, lat2: float, long2: float) -> float:
+    # compute distance between 2 GPS points using the haversine formula
+    # reference: https://www.movable-type.co.uk/scripts/latlong.html
+    lat1, long1, lat2, long2 = (math.radians(e) for e in (lat1, long1, lat2, long2))
+    a = (
+        math.sin((lat1 - lat2) / 2) ** 2
+        + math.cos(lat1) * math.cos(lat2) * math.sin((long1 - long2) / 2) ** 2
+    )
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return 6371e3 * c
+
+
+def speed_gps(loc1: Location, loc2: Location) -> float:
+    # compute the average speed between 2 locations (result in meters per second)
+    if loc1.timestamp == loc2.timestamp:
+        return 0.0
+    d = dist_gps(loc1.latitude, loc1.longitude, loc2.latitude, loc2.longitude)
+    t = abs(loc1.timestamp - loc2.timestamp)
+    return d / t  # m/s
+
+
+def discard_bad_gps_points(location_history: List[Location]) -> List[Location]:
+    log.info("discarding bad points from location history...")
+    location_history_clean = []
+    for i in range(len(location_history)):
+        if 0 < i < len(location_history) - 1:
+            # XXX:
+            # It turns out the Google Location history isn't perfect, and we can expect
+            # to find some bad GPS points. In my data, I found multiple examples of
+            # lonely outliers: location n is ok, location n+1 is be very very far from
+            # location n, and location n+2 is close to location n. In such cases, a
+            # simple fix can be to remove location n+1 from the history.
+            speed_keep = 3.6 * speed_gps(
+                location_history_clean[-1], location_history[i]
+            )
+            speed_discard = 3.6 * speed_gps(
+                location_history_clean[-1], location_history[i + 1]
+            )
+            # arbitrary threshold: 200km/h
+            if speed_keep > 200.0 and speed_keep > 5 * speed_discard:
+                continue
+
+        location_history_clean.append(location_history[i])
+
+    n_discarded = len(location_history) - len(location_history_clean)
+    if n_discarded > 0:
+        log.warning(f"discarded {n_discarded}/{len(location_history)} GPS points")
+
+    return location_history_clean
 
 
 def read_google_location_history(path: str) -> List[Location]:
@@ -136,6 +188,7 @@ def add_google_location_to_images(
     location_history_path: str, root_path: str, utc_offset: float
 ):
     location_history = read_google_location_history(location_history_path)
+    location_history = discard_bad_gps_points(location_history)
     timestamps = [loc.timestamp for loc in location_history]
     exif = read_exif(root_path)
     exif_diff = get_exif_diff(exif, location_history, utc_offset)
